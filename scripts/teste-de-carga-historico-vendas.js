f674de0b97983fd7a3913d9a74ab2983007e6cd5
Guilherme <willfkrause@outlook.com>
2024-08-06 12:29:46 -0300

    add new scripts and remove tests examples

---
+++ scripts/teste-de-carga-historico-vendas.js
@@ -0,0 +1,59 @@
+// Esse teste tem como propósito testar a requisição do histórico de vendas
+
+import http from "k6/http";
+import { sleep, check } from "k6";
+
+export const options = {
+  stages: [
+    { duration: "5s", target: 1 },
+    { duration: "40s", target: 30 },
+    { duration: "55s", target: 60 },
+    { duration: "1m15s", target: 100 },
+    { duration: "3m15s", target: 100 },
+    { duration: "1s", target: 0 },
+  ],
+};
+
+export function setup() {
+  const url = "https://api2.yooga.com.br/authenticate";
+  const payload = JSON.stringify({
+    login_api: "146.256.737-12",
+    senha_api: "humm3r28@*",
+  });
+
+  const params = {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  };
+
+  const login = http.post(url, payload, params);
+
+  check(login, {
+    "Status 200 Ok!": (r) => r.status === 200,
+    "IDI encontrado": (r) => r.body.includes("8405"),
+  });
+
+  let authToken = login.body;
+  authToken = JSON.parse(authToken);
+
+  return authToken.token;
+}
+
+export default function (authToken) {
+  const url = "https://api2.yooga.com.br/v2/vendas?page=1&inverse=true"
+
+  const carts = http.request("GET", url, null, {
+    headers: {
+      Authorization: `Bearer ${authToken}`,
+    },
+  });
+
+  sleep(1);
+
+  check(carts, {
+    "Status 200 Ok!": (r) => r.status === 200,
+    "Body Ok!": (r) => r.body.includes("data", "lastPage","page", "perPage","totais", "desconto", "valor", "total"),
+  });
+}
