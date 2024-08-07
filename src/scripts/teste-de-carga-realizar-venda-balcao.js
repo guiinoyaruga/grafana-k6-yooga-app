834470e6a24aa1fdfe8c6c07a2a9f0abcfdb091d
Guilherme <willfkrause@outlook.com>
2024-08-07 17:04:42 -0300

    adjusts test sell

--- src/scripts/teste-de-carga-realizar-venda-balcao.js
+++ src/scripts/teste-de-carga-realizar-venda-balcao.js
@@ -1,10 +1,11 @@
 // Esse teste tem como propósito testar a requisição de criação de uma venda no balcão
 import http from "k6/http";
-import { sleep, check } from "k6";
-import { uuidv4 } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
+import { sleep, check, group } from "k6";
+import { uuidv4 } from "https://jslib.k6.io/k6-utils/1.4.0/index.js";
 import Login from "../requests/login-request.js";
 const login = new Login();
 const randomUUID = uuidv4();
+const randomRequestId = uuidv4();
 
 export const options = {
   stages: [{ duration: "3s", target: 1 }],
@@ -17,61 +18,84 @@ export function setup() {
 }
 
 export default function (authToken) {
-  const url = "https://api2.yooga.com.br/vendas";
-  const payload = JSON.stringify({
-    cliente: null,
-    discount: 0,
-    has_tax: false,
-    observacao: null,
-    pagamento: [
-      {
-        adquirente: "",
-        codigo: 637660,
-        descricao: "Dinheiro",
-        movimenta_caixa: 1,
-        taxa: null,
-        tipo_nfce: 1,
-        uuid: randomUUID,
-        valor: 1,
-        valor_pago: 1,
-      },
-    ],
-    printer_uuid: null,
-    products:[
+  let codigoVenda
+
+  group("Realizar venda", () => {
+    const url = "https://api2.yooga.com.br/vendas";
+    const payload = JSON.stringify({
+      products: [
         {
-            codigo: 14307866,
-            composicoes: [],
-            observacao: null,
-            qty: 1,
-            valor_custo: 0,
-            valor_venda: 1,
-        }
-    ],
-    remaining: 0,
-    request_id: randomUUID,
-    tax: 0,
-    tax_value: 0,
-    ticket: null,
-    total: 1,
-    transactions: [],
-    troco: 0, 
-    usuario: null
+          codigo: 14307866,
+          valor_venda: 1,
+          qty: 1,
+          valor_custo: 0,
+          observacao: null,
+          composicoes: [],
+        },
+      ],
+      pagamentos: [
+        {
+          uuid: randomUUID,
+          codigo: 637660,
+          descricao: "Dinheiro",
+          valor: 3,
+          valor_pago: 3,
+          movimenta_caixa: 1,
+          taxa: null,
+          tipo_nfce: 1,
+          adquirente: "",
+        },
+      ],
+      total: 3,
+      discount: 0,
+      tax: 0,
+      has_tax: false,
+      observacao: null,
+      cliente: null,
+      remaining: 0,
+      troco: 0,
+      tax_value: 0,
+      request_id: randomRequestId,
+      fidelity_discount_value: 0,
+      transactions: [],
+      printer_uuid: null,
+      ticket: null,
+      fidelity: null,
+      fidelity_item_id: null,
+      created_at: "",
+      usuario: null,
+    });
+
+    const venda = http.post(url, payload, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json",
+      },
+    });
+
+    sleep(3);
+
+    codigoVenda = JSON.parse(venda.body)
+
+    check(venda, {
+      "Status 200 Ok!": (r) => r.status === 200,
+      "Venda realizada": (r) => r.body.includes(randomRequestId),
+    });
   });
 
-  const venda = http.post(url, payload, {
-    headers: {
-      Authorization: `Bearer ${authToken}`,
-      "Content-Type": "application/json",
+  group("Deletar venda", () => {
+    const url = `https://api2.yooga.com.br/vendas/${codigoVenda.codigo}?motivo=`;
 
-    },
-  });
+    const cancelamento = http.del(url, null, {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-  sleep(3);
-
-  console.log(venda)
-
-  check(venda, {
-    "Status 200 Ok!": (r) => r.status === 200,
-    // "Venda realizada": (r) => r.body.includes("PREPARING"),
-  });
+    check(cancelamento, {
+      "Status 200 Ok!": (r) => r.status === 200,
+      "Venda cancelada": (r) => r.body.includes("Venda cancelada com sucesso"),
+    })
+  })
 }
